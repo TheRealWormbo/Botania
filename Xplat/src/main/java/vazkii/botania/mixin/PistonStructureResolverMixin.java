fd5bd74ce0f6b66159554969780aa76fdebb58cf
TheRealWormbo <github@wormbo.de>
2024-03-30 21:47:45 +0100

    Cleaner implementation for PistonStructureResolverMixin

--- Xplat/src/main/java/vazkii/botania/mixin/PistonStructureResolverMixin.java
+++ Xplat/src/main/java/vazkii/botania/mixin/PistonStructureResolverMixin.java
@@ -1,30 +1,43 @@
 package vazkii.botania.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.piston.PistonStructureResolver;
 
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import vazkii.botania.common.helper.ForcePushHelper;
 
 @Mixin(value = PistonStructureResolver.class)
 public class PistonStructureResolverMixin {
-	// not final, due to access widener/transformer definition:
-	@SuppressWarnings("ShadowModifiers")
+	/**
+	 * The piston position. Needs to be defined as mutable final shadow field, otherwise access transformer/widener
+	 * entry is required for this mixin to work, even though its code doesn't appear to write to it.
+	 */
+	@Final
+	@Mutable
 	@Shadow
+	@SuppressWarnings("unused")
 	private BlockPos pistonPos;
 
 	/**
 	 * Since the pushing piston block is handled separately via its position,
 	 * replace it with the force relay's position when pushing blocks that way.
 	 */
-	@Inject(method = "<init>", at = @At(value = "RETURN"))
-	private void modifyForcePushOrigin(Level level, BlockPos pistonPos, Direction pistonDirection, boolean extending, CallbackInfo ci) {
-		this.pistonPos = ForcePushHelper.isForcePush() ? ForcePushHelper.getForcePushOrigin() : pistonPos;
+	@WrapOperation(
+		method = "<init>", at = @At(
+			value = "FIELD", opcode = Opcodes.PUTFIELD,
+			target = "Lnet/minecraft/world/level/block/piston/PistonStructureResolver;pistonPos:Lnet/minecraft/core/BlockPos;"
+		)
+	)
+	private void modifyForcePushOrigin(PistonStructureResolver instance, BlockPos value, Operation<Void> original) {
+		original.call(instance, ForcePushHelper.isForcePush() ? ForcePushHelper.getForcePushOrigin() : value);
 	}
 }
